0e15885432a2946cb5ac84033499b9d6ecc01ea7
EsaulovaLudmila <esaulova_l_v@mail.ru>
2023-12-06 12:01:17 +0300

    created_header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Header } from "./layout/header/Header";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+      <Header />
+      Hello, samurai! Let's go!
     </div>
   );
 }
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <a href="">
+        <img src="" alt="" />
+      </a>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Profile</a>
+          </li>
+          <li>
+            <a href="">Messages</a>
+          </li>
+          <li>
+            <a href="">News</a>
+          </li>
+          <li>
+            <a href="">Music</a>
+          </li>
+          <li>
+            <a href="">Setting</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
